5518bf00c13817c41601b3d7b5f4e6fd2b9f6140
Marcus <mboperator@gmail.com>
2018-05-30 19:51:06 -0700

    Added buttons

--- index.vr.js
+++ index.vr.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import {
   AppRegistry,
+  AmbientLight,
   asset,
   Pano,
+  Model,
   Text,
   View,
+  VrButton,
 } from 'react-vr';
 
 export default class SpaceVR extends React.Component {
+  state = {
+    rotation: 90,
+    zoomLevel: 0.0002,
+  }
+
+  rotate = () => {
+    this.setState({
+      rotation: this.state.rotation + 1,
+      zoomLevel: this.state.zoomLevel + 0.00001
+    })
+
+    requestAnimationFrame(this.rotate)
+  }
+
+  componentDidMount() {
+    this.rotate()
+  }
+
   render() {
     return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
+      <View style={{ flexDirection: 'row' }}>
+        <VrButton
           style={{
             backgroundColor: '#777879',
             fontSize: 0.8,
@@ -22,10 +42,66 @@ export default class SpaceVR extends React.Component {
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [{translate: [0, 1, -3]}],
           }}>
-          hello
-        </Text>
+          <Text>+</Text>
+        </VrButton>
+        <VrButton
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 1, -3]}],
+          }}>
+          <Text>-</Text>
+        </VrButton>
+        <AmbientLight intensity={2.5} />
+        <Model
+          style={{
+            transform: [
+              {translate: [-25, 0, -70]},
+              {scale: 0.05},
+              {rotateY: 90},
+              {rotateX: this.state.rotation},
+              {rotateZ: -10},
+            ]
+          }}
+          source={{
+            obj:asset('earth.obj'),
+            mtl: asset('earth.mtl'),
+          }}
+          lit={true}
+        />
+        <Model
+          style={{
+            transform: [
+              {translate: [10, 10, -100]},
+              {scale: 0.05},
+            ]
+          }}
+          lit={true}
+          source={{
+            obj:asset('moon.obj'),
+            mtl: asset('moon.mtl'),
+          }}
+        />
+        <Pano source={
+          {
+            uri: [
+              '../static_assets/space_right.png',
+              '../static_assets/space_left.png',
+              '../static_assets/space_up.png',
+              '../static_assets/space_down.png',
+              '../static_assets/space_front.png',
+              '../static_assets/space_back.png',
+            ]
+          }
+        } />
       </View>
     );
   }
